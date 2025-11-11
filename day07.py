eab8a18b0383bdfa3b25b3dcc21caafb278bff5b
jahearn3 <josephahearn3@gmail.com>
2025-11-11 10:30:02 -0800

    Day 7 complete

--- day07.py
+++ day07.py
@@ -7,100 +7,62 @@ f = os.path.basename(__file__)
 day = f[3:5]
 
 data = ld.load_data(f"example{day}.txt")
-# data = ld.load_data(f"input{day}.txt")
+data = ld.load_data(f"example{day}b.txt")
+data = ld.load_data(f"input{day}.txt")
 
 # Part 1
 bags = {}
-sgab = {}
 for line in data:
     k, v = line.split(' bags contain ')
     vv = v.strip('.').split(', ')
-    if 'no other bags' in vv:
-        bags[k] = []
-    else:
-        bags[k] = []
+    if vv[0] != 'no other bags':
         for i in range(len(vv)):
             words = vv[i].split(' ')
             color = ' '.join(words[1:3])
-            bags[k].append(color)
-            if color not in sgab:
-                sgab[color] = []
-            sgab[color].append(k)
+            if color not in bags:
+                bags[color] = []
+            bags[color].append(k)
 
-
-# for k, v in bags.items():
-#     print(k, v)
-
-# for k, v in sgab.items():
-#     print(k, v)
-
-# Go through dictionary from shiny gold
 valid_outermost_bags = []
-q = sgab['shiny gold']
+q = bags['shiny gold']
 
 while q:
     bag = q.pop(0)
-    # print('bag:', bag)
     if bag not in valid_outermost_bags:
-        # print('Adding', bag, 'to valid_outermost_bags')
         valid_outermost_bags.append(bag)
-    if bag in sgab:
-        for i in sgab[bag]:
-            # print('Adding', i, 'to q')
+    if bag in bags:
+        for i in bags[bag]:
             q.append(i)
-    # else:
-        # print(bag, 'not in sgab')
 
-# print(valid_outermost_bags)
 print(len(valid_outermost_bags))
 
 # Part 2
 bags = {}
-# sgab = {}
 for line in data:
     k, v = line.split(' bags contain ')
     vv = v.strip('.').split(', ')
     if 'no other bags' in vv:
-        bags[k] = []
+        bags[k] = [{'count': 0, 'color': None}]
     else:
         bags[k] = []
         for i in range(len(vv)):
             words = vv[i].split(' ')
             qty = int(words[0])
             color = ' '.join(words[1:3])
-            bags[k].append((color, qty))
-            # if color not in sgab:
-            #     sgab[color] = []
-            # sgab[color].append(k)
+            bags[k].append({'count': qty, 'color': color})
 
 
-for k, v in bags.items():
-    print(k, v)
+def bags_in_bags(bags, parent):
+    if parent is None:
+        return 0
+    else:
+        parent = bags[parent]
 
-# for k, v in sgab.items():
-#     print(k, v)
+    total = 0
+    for child in parent:
+        total += child['count'] * (bags_in_bags(bags, child['color']) + 1)
 
-# Go through dictionary from shiny gold
-seen = []
-bags_required = 1
-q = bags['shiny gold']
+    return total
 
-while q:
-    bag = q.pop(0)
-    (b, qty) = bag
-    # print('bag:', bag)
-    # if bag not in seen:
-    #     # print('Adding', bag, 'to valid_outermost_bags')
-    #     seen.append(bag)
-    # if bag in bags:
-    #     for i in sgab[bag]:
-    #         # print('Adding', i, 'to q')
-    #         q.append(i)
-    if len(bags[b]) > 0:
-        for i in bags[b]:
-            (bb, qt) = i
-            bags_required += (qty * qt)
-            print(b, bb, qty, qt)
-            q.append(i)
 
-print(bags_required)
+print(bags_in_bags(bags, 'shiny gold'))
